a3afe6756f55091c416501af562273785a312f88
ajiteshX <108587395+ajiteshX@users.noreply.github.com>
2025-03-10 14:01:37 +0530

    Update xyz.cpp

--- xyz.cpp
+++ xyz.cpp
@@ -1,17 +1,8 @@
-class Solution {
-    public:
-        bool isPowerOfThree(int n) {
-            if(n==0)
-                return false;
-            if(n==1)
-                return true;
-            
-            while(n%3==0)
-            {
-                n = n/3;
-                if(n==1)
-                    return true;
-            }
-            return false;
-        }
-    };
+// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
